f03841a9832fa5be8c3bd00cb6cda302976ce5a8
Ramjeevan Patha <ramjeevan2006@gmail.com>
2024-01-29 16:41:03 -0600

    Initial project structure commit

---
+++ desicuts-appointment-system/src/main/java/com/desicuts/DesicutsAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.desicuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesicutsAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesicutsAppointmentSystemApplication.class, args);
+	}
+
+}
